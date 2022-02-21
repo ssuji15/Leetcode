a2d7702159c06f6ebcedbb073ac3f4a3499a393b
SUJINDAR SELVARAJ <53935234+ssuji15@users.noreply.github.com>
2022-02-21 09:54:36 +0530

    Time: 260 ms (81.52%), Space: 131.8 MB (74.84%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        
-        deque<int> dq;
+        deque<int> sortedIndexes;
         vector<int> result;
-        
         for(int i=0;i<nums.size();i++) {
-
-            while(!dq.empty() && dq.front() < (i-(k-1))) {
-                dq.pop_front();
+            while(!sortedIndexes.empty() && sortedIndexes.front() < (i-(k-1))) {
+                sortedIndexes.pop_front();
             }
             
-            while(!dq.empty() && nums[i] > nums[dq.back()]) {
-                dq.pop_back();
+            while(!sortedIndexes.empty() && nums[i] > nums[sortedIndexes.back()]) {
+                sortedIndexes.pop_back();
             }
             
-            dq.push_back(i);
-            if(i - (k-1) >= 0) {
-                result.push_back(nums[dq.front()]);
+            sortedIndexes.push_back(i);
+            if(i-(k-1)>=0) {
+                result.push_back(nums[sortedIndexes.front()]);
             }
         }
-        
         return result;
     }
 };
