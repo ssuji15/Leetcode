fd9359739d9bd50f3f8ffd07df6938c3c573e0a5
SUJINDAR SELVARAJ <53935234+ssuji15@users.noreply.github.com>
2022-03-01 18:38:15 +0530

    Time: 116 ms (59.95%), Space: 8.4 MB (79.92%) - LeetHub

--- 301-remove-invalid-parentheses/301-remove-invalid-parentheses.cpp
+++ 301-remove-invalid-parentheses/301-remove-invalid-parentheses.cpp
@@ -8,41 +8,56 @@ public:
             result = {temp};
         }
         else if(result.begin()->length() == temp.length()) {
-            result.insert(temp);
+            
         }
     }
-    void findAll(int idx, string &s, int left, set<string> &result, string &temp) {
+    void findAll(int idx, string &s, int left, int leftRem, int rightRem,
+                 set<string> &result, string &temp) {
         if(idx == s.length()) {
-            if(left == 0) {
-                addInResult(result, temp);
+            if(left == 0 && leftRem == 0 && rightRem == 0) {
+                result.insert(temp);
             }
             return;
         }
         
         if(s[idx] == '(') {
-            findAll(idx+1, s, left, result, temp);
+            if(leftRem > 0) {
+                findAll(idx+1, s, left, leftRem-1, rightRem, result, temp);
+            }
+
             temp.push_back(s[idx]);
-            findAll(idx+1, s, left+1, result, temp);
+            findAll(idx+1, s, left+1, leftRem, rightRem, result, temp);
             temp.pop_back();
         }
         else if(s[idx] == ')') {
             if(left > 0) {
                 temp.push_back(s[idx]);
-                findAll(idx+1, s, left-1, result, temp);
+                findAll(idx+1, s, left-1, leftRem, rightRem, result, temp);
                 temp.pop_back();
             }
-            findAll(idx+1, s, left, result, temp);
+            
+            if(rightRem > 0) {
+                findAll(idx+1, s, left, leftRem, rightRem-1, result, temp);
+            }
         }
         else {
             temp.push_back(s[idx]);
-            findAll(idx+1, s, left, result, temp);
+            findAll(idx+1, s, left, leftRem, rightRem, result, temp);
             temp.pop_back();
         }
     }
     vector<string> removeInvalidParentheses(string s) {
         set<string> result;
         string temp = "";
-        findAll(0,s,0,result,temp);
+        int left = 0, right = 0;
+        for(int i=0;i<s.length();i++) {
+            if(s[i] == '(') left++;
+            else if(s[i] == ')') {
+                if(left > 0) left--;
+                else right++;
+            }
+        }
+        findAll(0,s,0,left,right,result,temp);
         vector<string> res(result.begin(), result.end());
         return res;
     }
