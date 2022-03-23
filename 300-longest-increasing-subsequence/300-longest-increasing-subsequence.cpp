ee64c035378de3b0612c953511983b03180d1178
SUJINDAR SELVARAJ <53935234+ssuji15@users.noreply.github.com>
2022-03-23 09:33:01 +0530

    Time: 300 ms (50.07%), Space: 10.6 MB (14.84%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -6,7 +6,7 @@ public:
         for(int i=1;i<nums.size();i++) {
             for(int j=0;j<i;j++) {
                 if(nums[j] < nums[i]) {
-                    dp[i] = max(dp[i], dp[j]+1);
+                    dp[i] = max(dp[i], dp[j] + 1);
                     ans = max(ans, dp[i]);
                 }
             }
