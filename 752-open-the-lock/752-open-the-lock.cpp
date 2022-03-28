eab2bf4e77df22f8c77f5add8641b7afc3402a78
SUJINDAR SELVARAJ <53935234+ssuji15@users.noreply.github.com>
2022-03-28 10:16:45 +0530

    Time: 203 ms (75.49%), Space: 37.7 MB (44.85%) - LeetHub

--- 752-open-the-lock/752-open-the-lock.cpp
+++ 752-open-the-lock/752-open-the-lock.cpp
@@ -1,57 +1,43 @@
 class Solution {
 public:
     int openLock(vector<string>& deadends, string target) {
-        unordered_map<string,bool> deadEndCache;
-        unordered_map<string,int> processedCache;
+        if(target.compare("0000") == 0) return 0;
+        unordered_map<string, bool> deadMap;
+        unordered_map<string, bool> visited;
         for(int i=0;i<deadends.size();i++) {
-            deadEndCache[deadends[i]] = true;
+            deadMap[deadends[i]] = true;
         }
-        int ans = INT_MAX;
         queue<pair<string,int>> q;
-        q.push({"0000",0});
+        q.push({"0000", 0});
+        visited["0000"] = true;
+        
         while(!q.empty()) {
             string cur = q.front().first;
-            int curDistance = q.front().second;
+            int steps = q.front().second;
             q.pop();
+            if(deadMap.find(cur) != deadMap.end()) continue;
             
-            if(deadEndCache.find(cur) != deadEndCache.end()) {
-                continue;
+            for(int i=0;i<cur.length();i++) {
+                string temp1(cur);
+                string temp2(cur);
+                if(temp1[i] == '0') { temp1[i] = '9';}
+                else temp1[i]--;
+                
+                if(temp2[i] == '9') { temp2[i] = '0';}
+                else temp2[i]++;
+                
+                if(temp1.compare(target) == 0 || temp2.compare(target) == 0) return steps+1;
+                
+                if(visited.find(temp1) == visited.end()) {
+                    visited[temp1] = true;
+                    q.push({temp1, steps+1});
+                }
+                if(visited.find(temp2) == visited.end()) {
+                    visited[temp2] = true;
+                    q.push({temp2, steps+1});
+                }
             }
-            
-            if(cur.compare(target) == 0) {
-                ans = min(ans, curDistance);
-                continue;
-            }
-
-            for(int i=0;i<4;i++) {
-                string temp = cur;
-                if(temp[i] == '0') {
-                    temp[i] = '9';
-                }
-                else {
-                    temp[i] = temp[i]-1;
-                }
-                
-                if(processedCache.find(temp) == processedCache.end() || processedCache[temp] > curDistance+1) {
-                    processedCache[temp] = curDistance + 1;
-                    q.push({temp,curDistance+1});
-                }
-                
-                temp = cur;
-                if(temp[i] == '9') {
-                    temp[i] = '0';
-                }
-                else {
-                    temp[i] = temp[i]+1;
-                }
-                
-                if(processedCache.find(temp) == processedCache.end() || processedCache[temp] > curDistance+1) {
-                    processedCache[temp] = curDistance + 1;
-                    q.push({temp,curDistance+1});
-                }
-            } 
         }
-        if(ans == INT_MAX) ans = -1;
-        return ans;
+        return -1;
     }
 };
