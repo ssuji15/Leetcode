607779b687de0f75e7c9530596128d2995ecb7f3
SUJINDAR SELVARAJ <53935234+ssuji15@users.noreply.github.com>
2022-03-29 19:08:42 +0530

    Time: 133 ms (80.23%), Space: 30 MB (67.56%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
@@ -1,56 +1,32 @@
 class Solution {
 public:
-
-    vector<vector<int>> directions = {{0,-1}, {-1,0}, {0,1}, {1,0}};
+    vector<vector<int>> directions = {{-1,0}, {0,-1}, {0,1}, {1,0}};
     int nearestExit(vector<vector<char>>& maze, vector<int>& entrance) {
-        int ans = INT_MAX;
-        queue<vector<int>> q;
-        vector<vector<int>> distance(maze.size(), vector<int>(maze[0].size(), INT_MAX));
+        queue<pair<int,int>> q;
+        q.push({entrance[0], entrance[1]});
         maze[entrance[0]][entrance[1]] = '+';
-        for(int i=0;i<maze.size();i++) {
-            if(maze[i][0] == '.') {
-                q.push({i,0});
-                distance[i][0] = 0;
-            }
-            if(maze[i][maze[0].size()-1] == '.') {
-                q.push({i,(int)maze[0].size()-1});
-                distance[i][maze[0].size()-1] = 0; 
-            }
-        }
-        
-        for(int i=1;i<maze[0].size()-1;i++) {
-            if(maze[0][i] == '.') {
-                q.push({0,i});
-                distance[0][i] = 0;
-            }
-            
-            if(maze[maze.size()-1][i] == '.') {
-                q.push({(int)maze.size()-1,i});
-                distance[(int)maze.size()-1][i] = 0;
-            }
-        }
-        maze[entrance[0]][entrance[1]] = '.';
+        int steps = 0, rows = maze.size(), cols = maze[0].size();
         while(!q.empty()) {
-            int i = q.front()[0];
-            int j = q.front()[1];
-            q.pop();
-
-            if(i == entrance[0] && j == entrance[1]) {
-                continue;
-            }
-    
-            for(int k=0;k<directions.size();k++) {
-                int newI = i + directions[k][0];
-                int newJ = j + directions[k][1];
-                
-                if(newI >= 0 && newI < maze.size() && newJ >= 0 && newJ < maze[0].size() 
-                  && maze[newI][newJ] == '.' && distance[newI][newJ] > 1 + distance[i][j]) {
-                    distance[newI][newJ] = 1 + distance[i][j];
-                    q.push({newI, newJ});
+            steps++;
+            int size = q.size();
+            while(size--) {
+                pair<int,int> pos = q.front();
+                q.pop();
+                for(int k=0;k<4;k++) {
+                    int newI = pos.first + directions[k][0];
+                    int newJ = pos.second + directions[k][1];
+                    if(newI >= 0 && newI < rows && newJ >= 0 && newJ < cols
+                       && maze[newI][newJ] == '.') {
+                        if(newI == 0 || newI == rows-1 || newJ == 0
+                           || newJ == cols-1) {
+                            return steps;
+                        }
+                        maze[newI][newJ] = '+';
+                        q.push({newI, newJ});
+                    }
                 }
             }
         }
-        if(distance[entrance[0]][entrance[1]] == INT_MAX) distance[entrance[0]][entrance[1]] = -1;
-        return distance[entrance[0]][entrance[1]];
+        return -1;
     }
 };
