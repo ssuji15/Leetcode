8c0baecab9a5195c6d4e71708507d139b4b082aa
SUJINDAR SELVARAJ <53935234+ssuji15@users.noreply.github.com>
2022-03-29 14:58:00 +0530

    Time: 381 ms (66.37%), Space: 90.4 MB (65.00%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.cpp
+++ 1905-count-sub-islands/1905-count-sub-islands.cpp
@@ -1,41 +1,27 @@
 class Solution {
 public:
-    vector<vector<int>> island;
-    vector<vector<bool>> visited;
     vector<vector<int>> directions = {{0,-1}, {-1,0}, {0,1}, {1,0}};
-    void dfs(int i, int j, vector<vector<int>> &grid) {
-        visited[i][j] = true;
-        island.push_back({i,j});
+    bool dfs(int i, int j, vector<vector<int>> & grid2, vector<vector<int>> &grid1) {
+        bool valid = grid1[i][j] == 1;
+        grid2[i][j] = 0;
         for(int k=0;k<directions.size();k++) {
             int newI = i + directions[k][0];
             int newJ = j + directions[k][1];
-            if(newI >= 0 && newI < grid.size() && newJ >= 0 && newJ < grid[0].size() && !visited[newI][newJ] && grid[newI][newJ] == 1) {
-                dfs(newI, newJ, grid);
+            
+            if(newI >= 0 && newI < grid2.size() && newJ >= 0 && newJ < grid2[0].size()
+               && grid2[newI][newJ] == 1) {
+                valid = valid & dfs(newI, newJ, grid2, grid1);
             }
         }
+        return valid;
     }
-    bool valid(vector<vector<int>> &grid) {
-        bool isValid = true;
-        for(int i=0;i<island.size();i++) {
-            if(grid[island[i][0]][island[i][1]] == 0) {
-                isValid = false;
-                break;
-            }
-        }
-        return island.size() > 0 && isValid;
-    }
+    
     int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
         int count = 0;
-        island = {};
-        visited = vector<vector<bool>>(grid2.size(), vector<bool>(grid2[0].size(), false));
         for(int i=0;i<grid2.size();i++) {
             for(int j=0;j<grid2[0].size();j++) {
-                if(grid2[i][j] == 1 && !visited[i][j]) {
-                    island = {};
-                    dfs(i,j,grid2);
-                    if(valid(grid1)) {
-                        count++;
-                    }
+                if(grid2[i][j] == 1) {
+                    if(dfs(i,j,grid2,grid1)) count++; 
                 }
             }
         }
