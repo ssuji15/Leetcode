eddabc27bc42ae134b0e6cebfdedf3c455c81716
SUJINDAR SELVARAJ <53935234+ssuji15@users.noreply.github.com>
2022-03-30 11:50:45 +0530

    Time: 20 ms (88.74%), Space: 7.3 MB (71.66%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,41 +1,41 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        vector<vector<bool>> dp(s.length(), vector<bool>(s.length(), false));
+        int length = 1, start = 0, end = 0;
         for(int i=0;i<s.length();i++) {
-            dp[i][i] = true;
-        }
-        int startIdx = 0, endIdx = 0, length = 1;
-        for(int i=1;i<s.length();i++) {
-            for(int j=0;j+i<s.length();j++) {
-                int x = j, y = j+i;
-                if(s[x] == s[y]) {
-                    if(y == x+1) {
-                        dp[x][y] = true;
-                    }
-                    else {
-                        dp[x][y] = dp[x+1][y-1];
-                    }
-                }
-                else {
-                    dp[x][y] = false;
-                }
-                
-                if(dp[x][y]) {
-                    int curLength = y-x+1;
-                    if(curLength > length) {
-                        length = curLength;
-                        startIdx = x;
-                        endIdx = y;
-                    }
-                }
+             int left = i, right = i;
+            while(left >= 0 && right < s.length() && s[left] == s[right]) {
+                left--;
+                right++;
+            }
+            
+            int curLength = right-left+1;
+            if(curLength > length) {
+                length = curLength;
+                start = left+1;
+                end = right-1;
+            }
+            
+            left = i;
+            right = i+1;
+
+            while(left >= 0 && right < s.length() && s[left] == s[right]) {
+                left--;
+                right++;
+            }
+            
+            curLength = right-left+1;
+            if(curLength > length) {
+                length = curLength;
+                start = left+1;
+                end = right-1;
             }
         }
-        string result = "";
-        while(startIdx <= endIdx) {
-            result += s[startIdx];
-            startIdx++;
+        
+        string res = "";
+        for(int i=start;i<=end;i++) {
+            res += s[i];
         }
-        return result;
+        return res;
     }
 };
