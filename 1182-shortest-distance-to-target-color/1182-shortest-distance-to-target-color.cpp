64060d4c76c64635d419c83fa8c613e50412bfd3
SUJINDAR SELVARAJ <53935234+ssuji15@users.noreply.github.com>
2022-03-31 09:10:17 +0530

    Time: 525 ms (47.62%), Space: 138.7 MB (34.92%) - LeetHub

--- 1182-shortest-distance-to-target-color/1182-shortest-distance-to-target-color.cpp
+++ 1182-shortest-distance-to-target-color/1182-shortest-distance-to-target-color.cpp
@@ -1,48 +1,33 @@
 class Solution {
 public:
     vector<int> shortestDistanceColor(vector<int>& colors, vector<vector<int>>& queries) {
-        vector<vector<int>> distances(colors.size(), vector<int>(3,INT_MAX));
+        int n = colors.size(), color1=INT_MAX, color2 = INT_MAX, color3 = INT_MAX;
+        vector<vector<int>> distance(n, vector<int>(3,INT_MAX));
         
-        int color1 = INT_MAX, color2 = INT_MAX, color3 = INT_MAX;
-        for(int i=0;i<colors.size();i++) {
-            if(colors[i] == 1) {
-                color1 = i;
-            }
-            else if(colors[i] == 2) {
-                color2 = i;
-            }
-            else if(colors[i] == 3) {
-                color3 = i;
-            }
-
-            distances[i][0] = color1 == INT_MAX ? INT_MAX : i-color1;
-            distances[i][1] = color2 == INT_MAX ? INT_MAX : i-color2;
-            distances[i][2] = color3 == INT_MAX ? INT_MAX : i-color3;
+        for(int i=0;i<n;i++) {
+            if(colors[i] == 1) color1 = i;
+            else if(colors[i] == 2) color2 = i;
+            else color3 = i;
+            distance[i][0] = color1 == INT_MAX ? INT_MAX : i-color1;
+            distance[i][1] = color2 == INT_MAX ? INT_MAX : i-color2;
+            distance[i][2] = color3 == INT_MAX ? INT_MAX : i-color3;
         }
-        color1 = INT_MAX, color2 = INT_MAX, color3 = INT_MAX;
-        for(int i=colors.size()-1;i>=0;i--) {
-            if(colors[i] == 1) {
-                color1 = i;
-            }
-            else if(colors[i] == 2) {
-                color2 = i;
-            }
-            else if(colors[i] == 3) {
-                color3 = i;
-            }
-            distances[i][0] = min(distances[i][0],color1 == INT_MAX ? INT_MAX : color1-i);
-            distances[i][1] = min(distances[i][1],color2 == INT_MAX ? INT_MAX : color2-i);
-            distances[i][2] = min(distances[i][2],color3 == INT_MAX ? INT_MAX : color3-i);
+        color1 = INT_MAX;color2 = INT_MAX;color3 = INT_MAX;
+        for(int i=n-1;i>=0;i--) {
+            if(colors[i] == 1) color1 = i;
+            else if(colors[i] == 2) color2 = i;
+            else color3 = i;
+            distance[i][0] = color1 == INT_MAX ? distance[i][0] : min(distance[i][0],color1-i);
+            distance[i][1] = color2 == INT_MAX ? distance[i][1] : min(distance[i][1],color2-i);
+            distance[i][2] = color3 == INT_MAX ? distance[i][2] : min(distance[i][2],color3-i);
         }
         
         vector<int> result;
         for(int i=0;i<queries.size();i++) {
-            int idx = queries[i][0];
-            int color = queries[i][1]-1;
-            if(distances[idx][color] == INT_MAX) {
-                distances[idx][color] = -1;
+            if(distance[queries[i][0]][queries[i][1]-1] == INT_MAX) {
+                distance[queries[i][0]][queries[i][1]-1] = -1;
             }
-            result.push_back(distances[idx][color]);
+            result.push_back(distance[queries[i][0]][queries[i][1]-1]);
         }
         
         return result;
