87034b20dc9c89989957db2dc2ef60ffe7e1d606
SUJINDAR SELVARAJ <53935234+ssuji15@users.noreply.github.com>
2022-03-31 09:26:07 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (48.75%) - LeetHub

--- 343-integer-break/343-integer-break.cpp
+++ 343-integer-break/343-integer-break.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
-
     int integerBreak(int n) {
-        vector<int> arr(n+1,0);
-        arr[0] = 0;
-        arr[1] = 0;
-        arr[2] = 1;
-        
+        vector<int> dp(n+1,0);
+        dp[2] = 1;
         for(int i=3;i<=n;i++) {
-            for(int j=1;j<i;j++) {
-                int remaining = i-j;
-                arr[i] = max(arr[i], max(j*(remaining), j * arr[remaining]));
+            int ans = INT_MIN;
+            for(int k=1;k<i;k++) {
+                ans = max(ans, max(k*(i-k),k * dp[i-k]));
             }
+            dp[i] = ans;
         }
-        return arr[n];
+        return dp[n];
     }
 };
