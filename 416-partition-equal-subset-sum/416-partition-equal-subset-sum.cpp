0260b8196369f5d547da95336642b5e1cb19910a
SUJINDAR SELVARAJ <53935234+ssuji15@users.noreply.github.com>
2022-04-05 19:50:51 +0530

    Time: 207 ms (56.69%), Space: 32.3 MB (28.52%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,22 +1,32 @@
 class Solution {
 public:
+    bool dfs(int idx, int targetSum, vector<int> &nums, unordered_map<int,unordered_map<int,int>> &cache) {
+        if(idx == nums.size() || targetSum < 0) return false;
+        
+        if(targetSum == 0) return true;
+        
+        if(cache.find(idx) != cache.end() && cache[idx].find(targetSum) != cache[idx].end()) {
+            return cache[idx][targetSum];
+        }
+        
+        cache[idx][targetSum] = dfs(idx+1, targetSum-nums[idx], nums, cache) || dfs(idx+1, targetSum, nums, cache);
+        
+        return cache[idx][targetSum];
+    }
     bool canPartition(vector<int>& nums) {
-        int totalSum = 0, subSetSum = 0;
+        unordered_map<int,unordered_map<int,int>> cache;
+        int targetSum = 0, sum = 0;
         
         for(int i=0;i<nums.size();i++) {
-            totalSum += nums[i];
+            targetSum += nums[i];
         }
-        if(totalSum % 2 != 0) return false;
-        subSetSum = totalSum / 2;
-        vector<vector<bool>> dp(nums.size()+1, vector<bool>(subSetSum+1, false)); 
-        dp[0][0] = true;
-        for(int i=1;i<=nums.size();i++) {
-            int curElement = nums[i-1];
-            for(int j=0;j<=subSetSum;j++) {
-                if(j<curElement) dp[i][j] = dp[i-1][j];
-                else dp[i][j] = dp[i-1][j] || dp[i-1][j-curElement];
-            }
-        }
-        return dp[nums.size()][subSetSum];
+        
+        if(targetSum % 2 != 0) return false;
+        
+        targetSum = targetSum / 2;
+        
+        return dfs(0,targetSum,nums,cache);
     }
 };
+
+
