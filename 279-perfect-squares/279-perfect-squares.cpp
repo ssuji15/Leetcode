dbf523ad2385d74c2c69b3f8f0dc1222ff598681
SUJINDAR SELVARAJ <53935234+ssuji15@users.noreply.github.com>
2022-04-05 17:26:10 +0530

    Time: 198 ms (57.89%), Space: 9.2 MB (44.85%) - LeetHub

--- 279-perfect-squares/279-perfect-squares.cpp
+++ 279-perfect-squares/279-perfect-squares.cpp
@@ -1,40 +1,15 @@
 class Solution {
 public:
-    int solve(int n, vector<int> &squareNumbers, unordered_map<int,int> &cache) {
-        
-        if(n == 1) return 1;
-        if(cache.find(n) != cache.end()) return cache[n];
-        auto idx = lower_bound(squareNumbers.begin(), squareNumbers.end(), n);
-        
-        if(*idx == n) return 1;
-        
-        if(*idx > n) {
-            idx--;
-        }
-        
-        if(idx == squareNumbers.begin()) {
-            return 1 + solve(n-1, squareNumbers, cache);
-        }
-        
-        int ans = INT_MAX;
-        while(idx != squareNumbers.begin()) {
-            int val = solve(n-*idx, squareNumbers, cache);
-            if(val != INT_MAX) val++;
-            ans = min(ans,val);
-            idx--;
-        }
-        cache[n] = ans;
-        return ans;
-    }
-    
     int numSquares(int n) {
-        vector<int> squareNumbers;
-        unordered_map<int,int> cache;
-        int i=1;
-        while(i*i <= 10000) {
-            squareNumbers.push_back(i*i);
-            i++;
+        vector<int> dp(n+1,0);
+        dp[1] = 1;
+        for(int i=2;i<=n;i++) {
+            int ans = INT_MAX;
+            for(int j=1;j*j<=i;j++) {
+                ans = min(ans, 1+dp[i-(j*j)]);
+            }
+            dp[i] = ans;
         }
-        return solve(n, squareNumbers, cache);
+        return dp[n];
     }
 };
