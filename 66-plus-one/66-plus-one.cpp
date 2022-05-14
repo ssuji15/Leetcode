c66ac6a008431bf0305712bae957433c6a21924e
SUJINDAR SELVARAJ <53935234+ssuji15@users.noreply.github.com>
2022-05-14 18:14:05 +0530

    Time: 0 ms (100.00%), Space: 8.7 MB (85.70%) - LeetHub

--- 66-plus-one/66-plus-one.cpp
+++ 66-plus-one/66-plus-one.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        vector<int> result;
-        int carry = 1;
+
         for(int i=digits.size()-1;i>=0;i--) {
-            int value = digits[i] + carry;
-            result.push_back(value%10);
-            carry = value/10;
+            if(digits[i] == 9) digits[i] = 0;
+            else {
+                digits[i]++;
+                return digits;
+            }
         }
-        if(carry != 0) {
-            result.push_back(carry);
-        }
-        reverse(result.begin(), result.end());
-        return result;
+        digits.insert(digits.begin(),1);
+        return digits;
     }
 };
